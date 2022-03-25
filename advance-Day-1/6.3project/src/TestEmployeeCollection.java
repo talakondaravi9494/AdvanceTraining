e5fc96dc0579fd3711eeca1798f405be21100fb0
talakondaravi9494 <55018803+talakondaravi9494@users.noreply.github.com>
2022-03-25 11:09:52 +0530

    Add files via upload

---
+++ advance-Day-1/6.3project/src/TestEmployeeCollection.java
@@ -0,0 +1,34 @@
+import java.util.Vector;
+public class TestEmployeeCollection {
+	public static void main(String[] args) {
+		Vector<Employee> v = addInput();
+		display(v);
+		}
+
+	private static Vector<Employee> addInput() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	private static void display(Vector<Employee> v) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public static void main1(String[] args) {
+		Employee e1=new Employee (101,"Ravi", "Teja");
+		Employee e2=new Employee (102,"Srinu", "Charan");
+		Employee e3=new Employee (103,"Shiva", "Sai");
+		Vector<Employee> v=new Vector<Employee>();
+		v.add(e1);
+		v.add(e2);
+		v.add(e3);
+		return v;
+	}
+	public static void main11(String[] args) {
+		for(Employee e:v)
+		{
+			System.out.println(e.getEmpid()+"\t"+e.getEname()+"\t"+e.getAddress());
+		}
+	}
+}
---
+++ advance-Day-1/7.2project/src/NameNotValidException.java
@@ -0,0 +1,7 @@
+class NameNotValidException extends Exception
+{
+     public String validname()
+     {
+          return ("Name is not Valid..Please ReEnter the Name");
+     }
+}
